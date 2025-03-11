d2601795c5bdfccd2986c02e823b48cccf5fe525
Hiddev666 <officehiddev@gmail.com>
2025-03-11 18:50:34 +0700

    add supabase

--- src/app.mjs
+++ src/app.mjs
@@ -1,6 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
 import multer from "multer"
+import { supabase } from "./supabase/supabase.mjs"
+import { decode } from "base64-arraybuffer"
 
 dotenv.config()
 const PORT = process.env.SERVER_PORT
@@ -9,7 +11,7 @@ const app = express()
 app.use(express.json())
 
 const storage = multer.memoryStorage()
-const upload = multer({storage: storage})
+const upload = multer({ storage: storage })
 
 app.get("/", (req, res) => {
     res.send({
@@ -21,11 +23,28 @@ app.post("/api/images", upload.single("file"), async (req, res) => {
     try {
         const file = req.file
 
-        if(!file) return res.status(400).send({
+        if (!file) return res.status(400).send({
             message: "Please upload a file"
         })
 
-        res.send(file)
+        const fileBase64 = decode(file.buffer.toString("base64"))
+
+        const { data, error } = await supabase.storage
+            .from("images")
+            .upload(file.originalname, fileBase64, {
+                contentType: "image/png",
+            })
+
+        if (error) throw error
+
+        const { data: image } = supabase.storage
+            .from("images")
+            .getPublicUrl(data.path)
+
+        console.log(file)
+        res.send({
+            image: image.publicUrl
+        })
     } catch (err) {
         res.send({
             message: err.message
